3a74f06e283cdaad42d56e6747eef81ada2029d3
Yisheng Cai <cai.eason@gmail.com>
2020-04-19 22:20:25 -0700

    Updates

---
+++ precache-manifest.b0e1b76961e3b5e5d75df31966801fa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172646e06e729cfd906aecbef0e4ddf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2cdafe6c72ad7271adf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae6267652a6db3516866",
+    "url": "/static/js/2.7ca7c0da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ca7c0da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2cdafe6c72ad7271adf",
+    "url": "/static/js/main.566871e6.chunk.js"
+  },
+  {
+    "revision": "ce5e35075cd86bceea05",
+    "url": "/static/js/runtime-main.8dd572a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
